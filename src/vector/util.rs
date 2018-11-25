290f0d6b9de585389ce282e5bf021f8f794db39c
cole <cole>
2018-11-25 05:53:34 +0000

    Shaped Vector assoc

--- src/vector/util.rs
+++ src/vector/util.rs
@@ -69,7 +69,7 @@ pub fn size(unit_count: u32) -> u32 {
     cap_at_arity(next_power(unit_count | 0x2))
 }
 
-pub fn path_width_stack(tailoff: u32, path: u32) -> u32 {
+pub fn path_widths(tailoff: u32, path: u32) -> u32 {
     let path = path & (!1u32); // makes bottom digit differ
     let last_index = tailoff - 1;
     let height = digit_count(last_index);
@@ -81,7 +81,7 @@ pub fn path_width_stack(tailoff: u32, path: u32) -> u32 {
     let bits = black_out_digit_count * BITS;
     let mask = (1 << bits) - 1;
     let path_widths = (last_index & (!mask)) | mask;
-    reverse_digits(path_widths, height)
+    path_widths
 }
 
 #[cfg(test)]
